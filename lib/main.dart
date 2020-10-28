0d01fabe2f4be003e497c19a926381d35268c494
Dao Hoang Son <daohoangson@gmail.com>
2020-10-28 22:16:43 +0700

    Call `initializeFirebase` in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:moneybook/src/data/firebase_app.dart';
 import 'package:moneybook/src/data/riverpod.dart';
 import 'package:moneybook/src/feat/home/home_page.dart';
 import 'package:moneybook/src/l10n/strings.dart';
@@ -8,6 +9,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences.getInstance();
+  initializeFirebase();
 
   runApp(MyApp());
 }
